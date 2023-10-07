24fdba0b5876f51a196abafd0661e998d818c975
Sumit9019 <123553959+Sumit9019@users.noreply.github.com>
2023-10-08 00:19:08 +0530

    Add files via upload

---
+++ Assignment 3.js
@@ -0,0 +1,8 @@
+// Create a canvas element
+var canvas = document.getElementById("game-canvas");
+
+// Get the canvas context
+var ctx = canvas.getContext("2d");
+
+// Draw a rectangle
+ctx.fillRect(10, 10, 100, 100);
